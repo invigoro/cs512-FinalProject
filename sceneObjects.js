a04e3c7eae62ce2bead29ad020d40c688f4d848a
Tim Wells <invigoro@gmail.com>
2025-12-01 11:47:58 -0700

    rear axle

--- sceneObjects.js
+++ sceneObjects.js
@@ -1,10 +1,30 @@
 const globalObjects = [];
 
-//Vehicle
-let kartBase = createCube(2);
-kartBase.scaY = 1;
+/* VEHICLE */
+let kartBase = createCube(.1);
 
-//Obstacles
+let chassis = createCube(2, [0.9, .02, 0.03]);
+chassis.scaY = 0.3;
+chassis.scaX = .75;
+chassis.scaZ = 1.5;
+kartBase.appendChild(chassis);
+
+let axleRear = createCylinder(.1, 2, 8, [0.3, 0.3, 0.3]);
+axleRear.rotX = Math.PI / 2;
+axleRear.rotY = Math.PI / 2;
+axleRear.posZ = 1;
+
+let wheelRL = createCylinder(.5,.25, 32, [0.15, 0.15, 0.15]);
+wheelRL.posY = -1;
+axleRear.appendChild(wheelRL);
+let wheelRR = createCylinder(.5,.25, 32, [0.15, 0.15, 0.15]);
+wheelRR.posY = 1;
+axleRear.appendChild(wheelRR);
+
+kartBase.appendChild(axleRear);
+
+
+/* OBSTACLES */
 let cube1 = createCube(4);
 cube1.setPos([-1, 1, -10]);
 cube1.setRot([0, 1, 0]);
